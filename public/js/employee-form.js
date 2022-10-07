2838a68080f3e2c09bf32e3cb3e05f4565be860d
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-10-07 15:23:35 +0200

    posta bild med POST form

--- public/js/employee-form.js
+++ public/js/employee-form.js
@@ -1,13 +1,21 @@
 const formTemplate = document.createElement("template");
 formTemplate.innerHTML = `
     <form>
+        <h2>Add Person</h2>
         <section>
-        
         </section>
         <input type="submit"/>
         <span id="result"></span>
     </form>
 `;
+
+const employeeFormLables = [
+  'name',
+  'email',
+  'phone',
+  'department_id',
+  null,
+];
 
 class EmployeeForm extends HTMLElement {
   constructor({ fields }) {
@@ -17,13 +25,19 @@ class EmployeeForm extends HTMLElement {
     this.shadowRoot.appendChild(formTemplate.content.cloneNode(true));
     this.form = this.shadowRoot.querySelector("form");
     this.inputs = this.form.querySelector("section");
-
+    this.form.setAttribute('enctype','multipart/form-data');
     this.result = this.form.querySelector("#result");
-
-    fields.forEach((element) => {
+    
+    fields.forEach((element,index) => {
       const input = document.createElement("input");
       input.setAttribute("type", element.fieldType);
       input.setAttribute("name", element.fieldName);
+      input.setAttribute('placeholder',employeeFormLables[index]);
+
+      if(index == 4){
+        input.setAttribute('accept','image/*');
+
+      }
 
       this.inputs.appendChild(input);
     });
@@ -41,23 +55,45 @@ class EmployeeForm extends HTMLElement {
   handleSubmit = async (e) => {
     e.preventDefault();
     const values = this.inputs.querySelectorAll("input");
+    let newFile = null;
 
     const body = {};
 
-    values.forEach((element) => {
+    values.forEach((element,index) => {
       const key = element.name;
       const value = element.value;
 
-      body[key] = value;
+      if(index == 4 && element.files.length > 0){
+        const file = element.files[0];
+        const blob = file.slice(0,file.size,'image/jpg');
+        const fileName = self.crypto.randomUUID();
+        newFile = new File([blob], fileName+".jpg", {type:"image/jpg"});
+
+        body[key] = fileName;
+
+      }else{
+        body[key] = value;
+      }
     });
 
     console.log(body);
+
+    if(newFile != null){
+      const data = new FormData();
+      data.append('file', newFile);
+
+      const imgResponse = await fetch('/api/img',{
+        method: "POST",
+        body: data,
+      })
+    }
+
     const response = await fetch("/api/employees", {
       method: "POST",
       body: JSON.stringify(body),
     });
-    const json = await response.json();
 
+    const json = await response.json();
     this.result.textContent = json.result;
 
     this.reset();
